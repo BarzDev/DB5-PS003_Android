3a9f42a74536701894dcbbe87d7f14f10650df78
akbarz <barz.develop@gmail.com>
2025-04-12 17:26:19 +0700

    refactor optimize import

--- app/src/main/java/com/example/android_db5_ps003/ui/screen/catalogue/CatalogueScreen.kt
+++ app/src/main/java/com/example/android_db5_ps003/ui/screen/catalogue/CatalogueScreen.kt
@@ -1,7 +1,6 @@
 package com.example.android_db5_ps003.ui.screen.catalogue
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
