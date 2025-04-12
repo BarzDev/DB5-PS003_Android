6c4376108f7fbed0686df2feda7d5824ad9be2e0
akbarz <barz.develop@gmail.com>
2025-04-12 20:41:05 +0700

    add: umkm main screen

---
+++ app/src/main/java/com/example/android_db5_ps003/ui/screen/umkm/UmkmScreen.kt
@@ -0,0 +1,71 @@
+package com.example.android_db5_ps003.ui.screen.umkm
+
+import android.app.Activity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.android_db5_ps003.R
+import com.example.android_db5_ps003.ui.navigation.Screen
+import com.example.android_db5_ps003.ui.screen.umkm_catalogue.UmkmCatalogueScreen
+
+@Composable
+fun UmkmScreen(
+    modifier: Modifier = Modifier,
+
+) {
+    Scaffold(
+        topBar = { TopBar() },
+        modifier = modifier
+        ) { innerPadding ->
+
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    val context = LocalContext.current
+
+    CenterAlignedTopAppBar(
+        title = { Text(stringResource(R.string.katalog_umkm)) },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = Color.White,
+        ),
+        modifier = Modifier,
+        navigationIcon = {
+            IconButton(onClick = { (context as? Activity)?.finish() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Localized description",
+                    tint = Color.White
+                )
+            }
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UmkmScreenPreview() {
+    UmkmScreen()
+}
