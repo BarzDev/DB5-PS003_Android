d6a63e595574cc4b1baebe87005ed97bc272f5b9
akbarz <barz.develop@gmail.com>
2025-04-13 18:56:58 +0700

    add: umkm detail view model

--- app/src/main/java/com/example/android_db5_ps003/ui/ViewModelFactory.kt
+++ app/src/main/java/com/example/android_db5_ps003/ui/ViewModelFactory.kt
@@ -5,18 +5,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.android_db5_ps003.data.repository.UmkmRepository
 import com.example.android_db5_ps003.ui.screen.umkm_catalogue.UmkmCatalogueViewModel
+import com.example.android_db5_ps003.ui.screen.umkm_detail.UmkmDetailViewModel
 
 class ViewModelFactory(private val repository: UmkmRepository) :
     ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return when {
-            modelClass.isAssignableFrom(UmkmCatalogueViewModel::class.java) -> UmkmCatalogueViewModel(
-                repository
-            ) as T
-
-            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+        if (modelClass.isAssignableFrom(UmkmCatalogueViewModel::class.java)) {
+            return UmkmCatalogueViewModel(repository) as T
+        } else if (modelClass.isAssignableFrom(UmkmDetailViewModel::class.java)) {
+            return UmkmDetailViewModel(repository) as T
         }
+
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
     }
 }
---
+++ app/src/main/java/com/example/android_db5_ps003/ui/screen/umkm_detail/UmkmDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.android_db5_ps003.ui.screen.umkm_detail
+
+import androidx.lifecycle.ViewModel
+import com.example.android_db5_ps003.data.repository.UmkmRepository
+
+class UmkmDetailViewModel(private val repository: UmkmRepository) : ViewModel() {
+}
