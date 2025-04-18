4e538d93c6dfd96d2245b0bfab2645742b4bb46e
Alif Khaerudin <alifkha7@live.com>
2025-04-19 03:32:03 +0700

    add: tourism screen route

--- app/src/main/java/com/example/android_db5_ps003/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/android_db5_ps003/ui/navigation/Screen.kt
@@ -3,5 +3,6 @@ package com.example.android_db5_ps003.ui.navigation
 sealed class Screen(val route: String) {
     data object Home : Screen("home")
     data object Catalogue : Screen("catalogue")
+    data object Tourism : Screen("catalogue/tourism")
     data object Emergency : Screen("emergency")
 }
