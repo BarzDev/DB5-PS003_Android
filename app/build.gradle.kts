afc634c56488320ce61921d60fd74eb7e92b154b
Alif Khaerudin <alifkha7@live.com>
2025-04-18 16:35:43 +0700

    feat: integrate coil compose library for image loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,6 @@ dependencies {
 
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
