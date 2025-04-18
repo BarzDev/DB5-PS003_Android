d8bddb81a8440ead084ec525fcb2b7caa75f498e
Alif Khaerudin <alifkha7@live.com>
2025-04-19 03:31:02 +0700

    feat: implement tourism screen with top bar and loading indicator

--- app/src/main/java/com/example/android_db5_ps003/ui/screen/tourism/TourismScreen.kt
+++ app/src/main/java/com/example/android_db5_ps003/ui/screen/tourism/TourismScreen.kt
@@ -1,17 +1,29 @@
 package com.example.android_db5_ps003.ui.screen.tourism
 
 import android.widget.Toast
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.android_db5_ps003.R
 import com.example.android_db5_ps003.data.remote.response.Tourism
 import com.example.android_db5_ps003.ui.ViewModelFactory
 import com.example.android_db5_ps003.ui.common.UiState
+import com.example.android_db5_ps003.ui.components.LoadingIndicator
 import com.example.android_db5_ps003.ui.components.TourismItem
 
 @Composable
@@ -20,19 +32,20 @@ fun TourismScreen(
     viewModel: TourismViewModel = viewModel(
         factory = ViewModelFactory.getInstance()
     ),
-    navigateToDetail: (Int) -> Unit
+    navigateBack: () -> Unit,
 ) {
     viewModel.uiState.collectAsStateWithLifecycle().value.let { uiState ->
         when (uiState) {
             is UiState.Loading -> {
                 viewModel.getAllTourism()
+                LoadingIndicator()
             }
 
             is UiState.Success -> {
                 TourismContent(
                     modifier = modifier,
                     tourism = uiState.data,
-                    navigateToDetail = navigateToDetail
+                    navigateBack = navigateBack
                 )
             }
 
@@ -43,24 +56,45 @@ fun TourismScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TourismContent(
     modifier: Modifier,
     tourism: List<Tourism>,
-    navigateToDetail: (Int) -> Unit
+    navigateBack: () -> Unit,
 ) {
-    LazyColumn(
-        modifier = modifier
-    ) {
-        items(tourism, key = { it.id }) { tourism ->
-            TourismItem(
-                image = tourism.img,
-                name = tourism.name,
-                rating = tourism.rating.toString(),
-                modifier = modifier.clickable {
-                    navigateToDetail(tourism.id)
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.tourism),
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navigateBack() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
                 }
             )
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = modifier.padding(innerPadding),
+        ) {
+            items(tourism, key = { it.id }) { tourism ->
+                TourismItem(
+                    image = tourism.img,
+                    name = tourism.name,
+                    rating = tourism.rating.toString(),
+                )
+            }
         }
     }
 }
