8df36176a9db9ec060ecb28e46a6f7cbe73eccdb
Alif Khaerudin <alifkha7@live.com>
2025-04-18 15:59:41 +0700

    feat: create tourism repository

---
+++ app/src/main/java/com/example/android_db5_ps003/data/repository/TourismRepository.kt
@@ -0,0 +1,35 @@
+package com.example.android_db5_ps003.data.repository
+
+import com.example.android_db5_ps003.data.remote.response.Tourism
+import com.example.android_db5_ps003.data.remote.retrofit.ApiService
+import com.example.android_db5_ps003.ui.common.UiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class TourismRepository(
+    private val apiService: ApiService
+) {
+    fun getAllTourism() : Flow<UiState<List<Tourism>>> {
+        return flow {
+            emit(UiState.Loading)
+            try {
+                val response = apiService.getTourism()
+                val events = response.data
+                emit(UiState.Success(events))
+            } catch (e: Exception) {
+                emit(UiState.Error(e.message.toString()))
+            }
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var instance: TourismRepository? = null
+        fun getInstance(
+            apiService: ApiService,
+        ): TourismRepository =
+            instance ?: synchronized(this) {
+                instance ?: TourismRepository(apiService)
+            }.also { instance = it }
+    }
+}
