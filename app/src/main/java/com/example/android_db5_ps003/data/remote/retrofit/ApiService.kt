aae2d814ffd1c753e958d4117d1e46926c372ee1
Alif Khaerudin <alifkha7@live.com>
2025-04-19 05:51:35 +0700

    feat: add tourism detail api service

--- app/src/main/java/com/example/android_db5_ps003/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/example/android_db5_ps003/data/remote/retrofit/ApiService.kt
@@ -1,9 +1,16 @@
 package com.example.android_db5_ps003.data.remote.retrofit
 
+import com.example.android_db5_ps003.data.remote.response.TourismDetailResponse
 import com.example.android_db5_ps003.data.remote.response.TourismResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("api/wisata")
     suspend fun getTourism(): TourismResponse
+
+    @GET("api/wisata/{id}")
+    suspend fun getTourismDetail(
+        @Path("id") id: Int
+    ): TourismDetailResponse
 }
