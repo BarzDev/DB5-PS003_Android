3073e25b52c3f2b75088b11a7d1ac9502aa02855
Alif Khaerudin <alifkha7@live.com>
2025-04-23 17:00:38 +0700

    refactor: remove example files

--- app/src/main/java/com/example/android_db5_ps003/data/remote/response/example.kt
+++
@@ -1,6 +0,0 @@
-package com.example.android_db5_ps003.data.remote.response
-
-
-// hapus file ini dan buat file baru sesuai kebutuham
-class example {
-}
--- app/src/main/java/com/example/android_db5_ps003/di/example.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android_db5_ps003.di
-
-// hapus file ini dan buat file baru sesuai kebutuham
-class example {
-}
