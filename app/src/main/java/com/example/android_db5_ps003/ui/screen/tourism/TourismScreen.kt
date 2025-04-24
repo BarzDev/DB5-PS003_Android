13c562a71df7d72153f525411a0cb199efce589a
Alif Khaerudin <alifkha7@live.com>
2025-04-25 02:35:20 +0700

    refactor: remove unnecessary navigation logic and update loading component in tourism screen

--- app/src/main/java/com/example/android_db5_ps003/ui/screen/tourism/TourismScreen.kt
+++ app/src/main/java/com/example/android_db5_ps003/ui/screen/tourism/TourismScreen.kt
@@ -8,22 +8,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -31,7 +23,7 @@ import com.example.android_db5_ps003.R
 import com.example.android_db5_ps003.data.remote.response.Tourism
 import com.example.android_db5_ps003.ui.TourismViewModelFactory
 import com.example.android_db5_ps003.ui.common.UiState
-import com.example.android_db5_ps003.ui.components.Loading
+import com.example.android_db5_ps003.ui.components.LoadingComponent
 import com.example.android_db5_ps003.ui.components.TourismItem
 import com.example.android_db5_ps003.ui.components.TourismSearchBar
 
@@ -41,7 +33,6 @@ fun TourismScreen(
     viewModel: TourismViewModel = viewModel(
         factory = TourismViewModelFactory.getInstance()
     ),
-    navigateBack: () -> Unit,
     navigateToDetail: (Int) -> Unit,
 ) {
     val query by viewModel.query
@@ -49,14 +40,13 @@ fun TourismScreen(
         when (uiState) {
             is UiState.Loading -> {
                 viewModel.getAllTourism()
-                Loading()
+                LoadingComponent()
             }
 
             is UiState.Success -> {
                 TourismContent(
                     modifier = modifier,
                     tourism = uiState.data,
-                    navigateBack = navigateBack,
                     navigateToDetail = navigateToDetail,
                     query = query,
                     onQueryChange = viewModel::search
@@ -75,38 +65,11 @@ fun TourismScreen(
 fun TourismContent(
     modifier: Modifier,
     tourism: List<Tourism>,
-    navigateBack: () -> Unit,
     navigateToDetail: (Int) -> Unit,
     query: String,
     onQueryChange: (String) -> Unit,
 ) {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(R.string.tourism),
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = { navigateBack() }
-                    ) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = null
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
-                )
-            )
-        }
-    ) { innerPadding ->
+    Scaffold { innerPadding ->
         LazyColumn(
             modifier = modifier.padding(innerPadding),
         ) {
