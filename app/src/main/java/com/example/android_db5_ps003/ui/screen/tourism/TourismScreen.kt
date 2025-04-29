36b6f88034c504c72327b49fda47e4f0a47b8ba3
akbarz <barz.develop@gmail.com>
2025-04-29 17:36:22 +0700

    fix: search bar position

--- app/src/main/java/com/example/android_db5_ps003/ui/screen/tourism/TourismScreen.kt
+++ app/src/main/java/com/example/android_db5_ps003/ui/screen/tourism/TourismScreen.kt
@@ -1,7 +1,9 @@
 package com.example.android_db5_ps003.ui.screen.tourism
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +24,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.android_db5_ps003.R
 import com.example.android_db5_ps003.data.remote.response.Tourism
-import com.example.android_db5_ps003.ui.TourismViewModelFactory
+import com.example.android_db5_ps003.ui.viewmodelfactory.TourismViewModelFactory
 import com.example.android_db5_ps003.ui.common.UiState
 import com.example.android_db5_ps003.ui.components.LoadingComponent
 import com.example.android_db5_ps003.ui.components.TourismItem
@@ -70,37 +73,43 @@ fun TourismContent(
     onQueryChange: (String) -> Unit,
 ) {
     Scaffold { innerPadding ->
-        LazyColumn(
+        Column (
             modifier = modifier.padding(innerPadding),
-        ) {
-            item {
-                TourismSearchBar(
-                    query = query,
-                    onQueryChange = onQueryChange
-                )
-                if (query.isNotEmpty()) {
-                    Row (
-                        modifier = modifier.padding(horizontal = 32.dp)
-                    ) {
-                        Text(
-                            text = tourism.size.toString(),
-                        )
-                        Spacer(modifier = modifier.width(4.dp))
-                        Text(
-                            text = stringResource(R.string.result),
-                        )
-                    }
+        ){
+            TourismSearchBar(
+                query = query,
+                onQueryChange = onQueryChange,
+                modifier = modifier
+                    .background(
+                        MaterialTheme.colorScheme.surface
+                    )
+                    .padding(bottom = 4.dp)
+            )
+            if (query.isNotEmpty()) {
+                Row (
+                    modifier = modifier.padding(horizontal = 32.dp)
+                ) {
+                    Text(
+                        text = tourism.size.toString(),
+                    )
+                    Spacer(modifier = modifier.width(4.dp))
+                    Text(
+                        text = stringResource(R.string.result),
+                    )
                 }
             }
-            items(tourism, key = { it.id }) { tourism ->
-                TourismItem(
-                    image = tourism.img,
-                    name = tourism.name,
-                    rating = tourism.rating.toString(),
-                    modifier = modifier.clickable(
-                        onClick = { navigateToDetail(tourism.id) }
+
+            LazyColumn() {
+                items(tourism, key = { it.id }) { tourism ->
+                    TourismItem(
+                        image = tourism.img,
+                        name = tourism.name,
+                        rating = tourism.rating.toString(),
+                        modifier = modifier.clickable(
+                            onClick = { navigateToDetail(tourism.id) }
+                        )
                     )
-                )
+                }
             }
         }
     }
